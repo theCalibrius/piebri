ef1d7c5d2bda26ab163f0db0773832b28d39aee0
Brian Pierce <brian@wanderfund.com>
2018-03-11 00:27:38 -0500

    Adjust imports

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 var Header = (props) => (
   <div className="Header">
