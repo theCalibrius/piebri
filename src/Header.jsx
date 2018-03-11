35bd88314a25c49379655ebe5fd8522c5aa81bb8
Brian Pierce <brian@wanderfund.com>
2018-03-11 01:44:16 -0500

    update font link and styles

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ var Header = (props) => (
   <div className="Header">
     <Link to='/' style={{ textDecoration: 'none', color: 'black' }}>
       <span class="navText">piebri:~$</span>
-      <span class="blinking-cursor"> 🁢</span>
+      <span class="blinking-cursor">🁢</span>
     </Link>
     <Link to='/projects' style={{ textDecoration: 'none', color: 'black' }}>     <span class="navText navLink">projects</span>  </Link>
     <Link to='/contact' style={{ textDecoration: 'none', color: 'black' }}>   <span class="navText navLink">contact</span>  </Link>
