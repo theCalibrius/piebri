963ea436c5a9cbd04c4e6134fabbd2424704a1e6
Brian Pierce <brian@wanderfund.com>
2018-03-11 01:30:32 -0500

    Create simple Header, logo, navLinks and add basic styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom';
 
 var Header = (props) => (
   <div className="Header">
-    <Link to='/'>Home </Link>
-    <Link to='/projects'>Projects </Link>
-    <Link to='/contact'>Contact</Link>
+    <Link to='/' style={{ textDecoration: 'none', color: 'black' }}>
+      <span class="navText">piebri:~$</span>
+      <span class="blinking-cursor"> 🁢</span>
+    </Link>
+    <Link to='/projects' style={{ textDecoration: 'none', color: 'black' }}>     <span class="navText navLink">projects</span>  </Link>
+    <Link to='/contact' style={{ textDecoration: 'none', color: 'black' }}>   <span class="navText navLink">contact</span>  </Link>
   </div>
 );
 
