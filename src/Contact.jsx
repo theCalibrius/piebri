c9fa070412adad104683abc1200fcd742104b054
Brian Pierce <brian@wanderfund.com>
2018-03-12 00:37:40 -0500

    Set up contact component

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 
 var Contact = (props) => (
   <div className="Contact">
-    This is the contact page, found in the Contact component.
+    <form method="post">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>Name: <input type="text" name="name"/></label>
+      </p>
+      <p>
+      	<label>Email: <input type="email" name="email"/></label>
+      </p>
+      <p>
+      	<label>Message: <textarea type="text" name="message"></textarea></label>
+      </p>
+       <p>
+      	<button type="submit">Submit</button>
+      </p>
+    </form>  
   </div>
 );
 
