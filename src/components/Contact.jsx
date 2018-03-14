2bfb547a75cf492a498ac7b31edf3f60d1ce8e0a
Brian Pierce <brian@wanderfund.com>
2018-03-13 19:37:14 -0500

    Add basic styling to projects and contact sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Row, Col } from 'reactstrap';
 
 var Contact = (props) => (
-  <div className="Contact">
-    <form method="post">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label>Name: <input type="text" name="name"/></label>
-      </p>
-      <p>
-      	<label>Email: <input type="email" name="email"/></label>
-      </p>
-      <p>
-      	<label>Message: <textarea type="text" name="message"></textarea></label>
-      </p>
-       <p>
-      	<button type="submit">Submit</button>
-      </p>
-    </form>  
-  </div>
+  <Container>  
+    <Row className="nameText">
+      <Col className="text-center">
+      <span>Contact</span>
+      </Col>
+    </Row>  
+    <Row className="contactForm">
+      <Col className="text-center"> 
+      <form method="post">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <input type="text" placeholder="Name" name="name"/>
+        </p>
+        <p>
+          <input type="email" placeholder="E-mail" name="email"/>
+        </p>
+        <p>
+          <textarea type="text" placeholder="Message" name="message"></textarea>
+        </p>
+         <p>
+          <button type="submit">Submit</button>
+        </p>
+      </form> 
+      </Col>
+    </Row>
+  </Container>
+
 );
 
 export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Row, Col } from 'reactstrap';
 
 var Projects = (props) => (
-  <div className="Projects">
-    This is the Projects page, from the Projects component.
-  </div>
+  <Container>  
+    <Row className="nameText">
+      <Col className="text-center">
+      <span>Projects</span>
+      </Col>
+    </Row> 
+    <Row className="projects homeText">
+      <Col className="text-center">
+      <span>ACCESS DENIED:  Whoops, looks like you're not logged in!</span>
+      </Col>
+    </Row>   
+  </Container>
 );
 
 export default Projects;
