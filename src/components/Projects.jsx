4aeefa2a726456eb2552bdacab95489ef4117e97
Brian Pierce <brian@wanderfund.com>
2018-03-18 14:03:50 -0500

    update Contact section and Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/Projects.css';
 
 
-var Projects = (props) => (
-  <div className="projectsContent">
-    <div className='title'>Projects</div>
-    <div className="leftProjects">
-        <li>tableCRM</li>
-        <li>noComments</li>
-        <li>pieBri</li>
-        <li>wanderFund</li>
-        <li>dlmfIt</li>
-    </div>
-    <div className="rightProjects">
-        <li>enGauge</li>
-        <li>marvelShake</li>
-        <li>fitStop</li>
-        <li>talkRight</li>
-        <li>docAdemy</li>
-    </div>
-  </div>
-);
+class Projects extends Component {
+  render() {
+    return(
+      <div className="projectsContent">
+        <div className='title'>Projects</div>
+        <div className="leftProjects">
+            <li>tableCRM</li>
+            <li>noComments</li>
+            <li>pieBri</li>
+            <li>wanderFund</li>
+            <li>dlmfIt</li>
+        </div>
+        <div className="rightProjects">
+            <li>enGauge</li>
+            <li>marvelShake</li>
+            <li>fitStop</li>
+            <li>talkRight</li>
+            <li>docAdemy</li>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default Projects;
