fc83b8e2dc8c859286f60bc26d0cc372e646297a
Brian Pierce <brian@wanderfund.com>
2018-03-26 22:00:45 -0500

    add more to projectModal component

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -5,13 +5,21 @@ class ProjectModal extends Component {
   constructor() {
     super()
     this.state = {
-
+      selectedProject: {}
+      showModal: false
     }
   }
 
   render() {
     return(
-      
+      <div className="modal">
+        <div className="projectTitle" title={this.state.selectedProject.title}></div>
+        <div className="closeIcon"></div>
+        <div className="projectDescription" description={this.state.selectedProject.description}></div>
+        <div className="githubButton" gitHubURL={this.state.selectedProject.girHubLink}></div>
+        <div className="liveProjectButton" liveURL={this.state.selectedProject.liveURL}></div>
+      </div>
+      <div className="shadow"></div>
     );
   }
 }
