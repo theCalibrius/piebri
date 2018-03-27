0ffd3f40d2896c2ab5cae7a304c6d16a6258d1c1
Brian Pierce <brian@wanderfund.com>
2018-03-26 19:17:23 -0500

    set up basic structure for component

---
+++ src/components/ProjectModal.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class ProjectModal extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      
+    );
+  }
+}
+
+
+export default ProjectModal;
