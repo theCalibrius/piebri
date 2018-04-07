c6eeab3ea6416d18652cd286ad2b464f9123338d
Brian Pierce <brian@wanderfund.com>
2018-04-07 16:34:06 -0500

    fix rendering of list items

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,81 +19,83 @@ class Projects extends Component {
         gitHubURL: 'http://www.gitHub.com',
         liveURL: 'http://www.live.com'
       },
-      leftColumnProjects: {
-        tableCRM: {
+      leftColumnProjects: [
+        {
           title: 'tableCRM',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         },
-        noComments: {
+        {
           title: 'noComments',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         },
-        pieBri: {
+        {
           title: 'pieBri',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         },
-        wanderFund: {
+        {
           title: 'wanderFund',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         },
-        dlmfIt: {
+        {
           title: 'dlmfIt',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         }
-      },
-      rightColumnProjects: {
-        enGauge: {
+      ],
+      rightColumnProjects: [
+        {
           title: 'enGauge',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         },
-        marvelShake: {
+        {
           title: 'marvelShake',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         },
-        fitStop: {
+        {
           title: 'fitStop',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         },
-        talkRight: {
+        {
           title: 'talkRight',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         },
-        docAdemy: {
+        {
           title: 'docAdemy',
           description: 'Table based CRM project built in React',
           gitHubURL: 'https://github.com/theCalibrius/TableCRM',
           liveURL: 'https://github.com/theCalibrius/TableCRM'
         }
-      }
+      ]
     };
 
     this.onClick = this.onClick.bind(this);
     // this.updateCurrentProject = this.updateCurrentProject.bind(this);
   } 
 
-  onClick = (id, e) => {
+  onClick = (projectsColumn, projectIndex, e) => {
     console.log('event: ', e);
-    console.log('id': id);
+    console.log('index': projectIndex);
+    console.log('column': projectsColumn);
     this.setState({
-      visible: true
+      visible: true,
+      selectedProject: this.state[projectsColumn][projectIndex]
     });
   }
 
@@ -136,26 +138,27 @@ class Projects extends Component {
                    <Button label="GitHub" onClick={() => {window.open(getLinkURL('gitHubURL'), "_blank")}} icon="fa-check" />
                    <Button label="Live App" onClick={() => {window.open(getLinkURL('liveURL'), "_blank")}} icon="fa-close" />
                  </div>
+    let leftColumnProjects = this.state.leftColumnProjects;
+    let rightColumnProjects = this.state.rightColumnProjects;
+
+     var leftProjects = leftColumnProjects.map((project, index) => {
+        let boundProjectClick = this.onClick.bind(this, "leftColumnProjects", index);
+        return (<li key={index} onClick={boundProjectClick}>{project.title}</li>);
+      });
+
+     var rightProjects = rightColumnProjects.map((project, index) => {
+        let boundProjectClick = this.onClick.bind(this, "rightColumnProjects", index);
+        return (<li key={index} onClick={boundProjectClick}>{project.title}</li>);
+      });
 
     return(
       <div className="projectsContent">
         <div className='title'>Projects</div>
         <div className="leftProjects">
-          {() => {
-
-          }}
-            <li onClick={(e) => {this.onClick(id, e)}}>tableCRM</li>
-            <li>noComments</li>
-            <li>pieBri</li>
-            <li>wanderFund</li>
-            <li>dlmfIt</li>
+          {leftProjects}
         </div>
         <div className="rightProjects">
-            <li>enGauge</li>
-            <li>marvelShake</li>
-            <li>fitStop</li>
-            <li>talkRight</li>
-            <li>docAdemy</li>
+          {rightProjects}
         </div>
       <Dialog header={this.state.selectedProject.title} description={this.state.selectedProject.description} visible={this.state.visible} footer={footer} width="350px" modal={true} onHide={this.onHide}>
         {getDescription()}
