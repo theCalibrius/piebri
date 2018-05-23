2a00a01fd7baea1043436056cc6c2ade62650cbe
Brian Pierce <brian@wanderfund.com>
2018-05-22 21:29:43 -0500

    clean up code style in new component

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -13,17 +13,9 @@ class Terminal extends Component {
       firstPrompt: true
     }
 
-
-    // this.showCursor = this.showCursor.bind(this);
-    // this.hideCursor = this.hideCursor.bind(this);
-    // this.triggerInputFocus = this.triggerInputFocus.bind(this);
-
   }
 
   componentDidMount() {
-
-    // this.input("Hello", function() {console.log('here it is');})
-    // console.log('this: ', this);
     this.getUserName = this.getUserName.bind(this);
     this.getUserEmail = this.getUserEmail.bind(this);
     this.getUserMessage = this.getUserMessage.bind(this);
@@ -37,16 +29,15 @@ class Terminal extends Component {
   }
 
 
-/*----------------------------------------------------------------------------*/
-/*------------------------ TERMINAL CLASS METHODS ----------------------------*/
-/*----------------------------------------------------------------------------*/
+  /*--------------------------------------------------------------------------*/
+  /*------------------------ TERMINAL CLASS METHODS --------------------------*/
+  /*--------------------------------------------------------------------------*/
 
   configureTerminalWindow() {
     this.setBackgroundColor('#FCFCFC');
     this.setHeight('400px');
     this.setTextSize('0.9em');
     this.setTextColor('#232D2D');
-    // this.setBorder('thin', 'solid', 'black', '4px');
     this.setLineHeight('160%');
   }
 
@@ -105,16 +96,10 @@ class Terminal extends Component {
     setTimeout(callback, milliseconds);
   }
 
-  // Below is my own custom method
-  // blinkingCursor(bool) {
-  //   const boolUpperCase = bool.toString().toUpperCase();
-  //   this._shouldBlinkCursor = (boolUpperCase === 'TRUE' || boolUpperCase === '1' || boolUpperCase === 'YES');
-  // }
 
-
-/*----------------------------------------------------------------------------*/
-/*------------------------ HELPER METHODS ----------------------------------*/
-/*----------------------------------------------------------------------------*/
+  /*--------------------------------------------------------------------------*/
+  /*------------------------ HELPER METHODS ----------------------------------*/
+  /*--------------------------------------------------------------------------*/
 
 
   fireCursorBlinkInterval() {
@@ -145,8 +130,6 @@ class Terminal extends Component {
 		this.refs.visibleUserInput.textContent = '';
 		this.refs.input.style.display = 'block';
     this.refs.terminalWrapper.appendChild(hiddenInputField);
-
-		// this.fireCursorBlinkInterval();
 
 		if ( message.length ) {
 		  this.print(PROMPT_TYPE === 'PROMPT_CONFIRM' ? message + ' (y/n)' : message);
@@ -207,204 +190,132 @@ class Terminal extends Component {
 	};
 
 
-/*----------------------------------------------------------------------------*/
-/*----------------------------CONTACT FORM METHODS----------------------------*/
-/*----------------------------------------------------------------------------*/
+  /*--------------------------------------------------------------------------*/
+  /*----------------------------CONTACT FORM METHODS--------------------------*/
+  /*--------------------------------------------------------------------------*/
 
 
-getUserName() {
-  return new Promise((resolve) => {
-    this.clear();
-    this.input("Please enter your name: ", (userInput) => {
-      this.confirm("Correct? ", (response) => {
-        if (response === true) {
-          this.clear();
-          this.setState({name: userInput});
-          resolve();
-        } else {
-          this.clear();
-          this.getUserName().then(resolve);
-        }
+  getUserName() {
+    return new Promise((resolve) => {
+      this.clear();
+      this.input("Please enter your name: ", (userInput) => {
+        this.confirm("Correct? ", (response) => {
+          if (response === true) {
+            this.clear();
+            this.setState({name: userInput});
+            resolve();
+          } else {
+            this.clear();
+            this.getUserName().then(resolve);
+          }
+        });
       });
     });
-  });
-}
+  }
 
-isEmailValid(addressToValidate) {
-  return new Promise((resolve) => {
-    axios.post('https://7ljv2tt4yl.execute-api.us-east-1.amazonaws.com/prod/ValidateEmailAddress', {
-      address: addressToValidate
-    })
-    .then((response) => {
-      resolve(response.data.format_valid && response.data.mx_found)
-    }).catch((error) => {
-      resolve(true);
-    });
-  });
-}
-
-getUserEmail() {
-  return new Promise((resolve) => {
-    this.input("Please enter your email: ", (userInput) => {
-      const alertValidating = document.createElement('span');
-      alertValidating.textContent = 'validating...'
-      this.refs.output.appendChild(alertValidating);
-      this.isEmailValid(userInput).then((returnValue) => {
-        this.refs.output.removeChild(alertValidating);
-        if (returnValue === true) {
-          this.confirm("Correct? ", (response) => {
-            if (response === true) {
-              this.clear();
-              this.setState({email: userInput});
-              resolve();
-            } else {
-              this.clear();
-              this.getUserEmail().then(resolve);
-            }
-          });
-        } else {
-          this.clear();
-          this.print("Not a valid email address");
-          this.getUserEmail().then(resolve);
-        }
+  isEmailValid(addressToValidate) {
+    return new Promise((resolve) => {
+      axios.post('https://7ljv2tt4yl.execute-api.us-east-1.amazonaws.com/prod/ValidateEmailAddress', {
+        address: addressToValidate
       })
+      .then((response) => {
+        resolve(response.data.format_valid && response.data.mx_found)
+      }).catch((error) => {
+        resolve(true);
+      });
     });
-  });
-}
+  }
 
-getUserMessage() {
-  return new Promise((resolve) => {
-    this.input("Please enter your message: ", (userInput) => {
-      this.confirm("Correct? ", (response) => {
+  getUserEmail() {
+    return new Promise((resolve) => {
+      this.input("Please enter your email: ", (userInput) => {
+        const alertValidating = document.createElement('span');
+        alertValidating.textContent = 'validating...'
+        this.refs.output.appendChild(alertValidating);
+        this.isEmailValid(userInput).then((returnValue) => {
+          this.refs.output.removeChild(alertValidating);
+          if (returnValue === true) {
+            this.confirm("Correct? ", (response) => {
+              if (response === true) {
+                this.clear();
+                this.setState({email: userInput});
+                resolve();
+              } else {
+                this.clear();
+                this.getUserEmail().then(resolve);
+              }
+            });
+          } else {
+            this.clear();
+            this.print("Not a valid email address");
+            this.getUserEmail().then(resolve);
+          }
+        })
+      });
+    });
+  }
+
+  getUserMessage() {
+    return new Promise((resolve) => {
+      this.input("Please enter your message: ", (userInput) => {
+        this.confirm("Correct? ", (response) => {
+          if (response === true) {
+            this.clear();
+            this.setState({message: userInput});
+            resolve();
+          } else {
+            this.clear();
+            this.getUserMessage().then(resolve);
+          }
+        });
+      });
+    });
+  }
+
+  confirmAndSend() {
+    return new Promise((resolve, reject) => {
+      this.print("Name: " + this.state.name);
+      this.print("Email: " + this.state.email);
+      this.print("Message: " + this.state.message);
+      this.print("--------------------");
+      this.confirm("Send your message?", (response) => {
         if (response === true) {
           this.clear();
-          this.setState({message: userInput});
           resolve();
         } else {
-          this.clear();
-          this.getUserMessage().then(resolve);
+          reject();
         }
       });
     });
-  });
-}
+  }
 
-confirmAndSend() {
-  return new Promise((resolve, reject) => {
-    this.print("Name: " + this.state.name);
-    this.print("Email: " + this.state.email);
-    this.print("Message: " + this.state.message);
-    this.print("--------------------");
-    this.confirm("Send your message?", (response) => {
-      if (response === true) {
-        this.clear();
-        resolve();
-      } else {
-        reject();
-      }
+  sendMessage() {
+    this.print("Sending Your Message...");
+    axios.post('https://u9udukv8j0.execute-api.us-east-1.amazonaws.com/prod/ContactFormLambda', {
+      "name": this.state.name,
+      "email": this.state.email,
+      "message": this.state.message
+    }).then((response) => {
+      this.clear();
+      this.print("Your message was succesfully sent.");
+    }).catch((error) => {
+      this.clear();
+      this.print("There was an error sending your message.");
     });
-  });
-}
+  }
 
-sendMessage() {
-  this.print("Sending Your Message...");
-  axios.post('https://u9udukv8j0.execute-api.us-east-1.amazonaws.com/prod/ContactFormLambda', {
-    "name": this.state.name,
-    "email": this.state.email,
-    "message": this.state.message
-  }).then((response) => {
-    this.clear();
-    this.print("Your message was succesfully sent.");
-  }).catch((error) => {
-    this.clear();
-    this.print("There was an error sending your message.");
-  });
-}
-
-gatherDataAndSend() {
-  this.getUserName()
-  .then(this.getUserEmail)
-  .then(this.getUserMessage)
-  .then(this.confirmAndSend)
-  .then(this.sendMessage, this.gatherDataAndSend);
-}
-
-/*----------------------------------------------------------------------------*/
-/*-------------------------- EVENT HANDLERS ----------------------------------*/
-/*----------------------------------------------------------------------------*/
+  gatherDataAndSend() {
+    this.getUserName()
+    .then(this.getUserEmail)
+    .then(this.getUserMessage)
+    .then(this.confirmAndSend)
+    .then(this.sendMessage, this.gatherDataAndSend);
+  }
 
 
-  // showCursor() {
-  //   this.refs.hiddenInputField.value = this.refs.visibleUserInput.textContent;
-  //   this.refs.cursor.style.display = 'inline';
-  // }
-  //
-  // hideCursor() {
-  //   this.refs.cursor.style.display = 'none';
-  // }
-  //
-  // triggerInputFocus() {
-  //   this.refs.hiddenInputField.focus();
-  // }
-  //
-  // handleKeyDown(e) {
-  //   if ( e.which === 37 || e.which === 39 || e.which === 38 || e.which === 40 || e.which === 9 ) {
-  //     e.preventDefault();
-  //   } else if ( shouldDisplayInput && e.which !== 13 ) {
-  //     setTimeout( () => {
-  //       this.refs.visibleUserInput.textContent = this.refs.hiddenInputField.value;
-  //     }, 1);
-  //   }
-  // }
-  //
-  // handleKeyUp(e) {
-  //   let inputValue;
-  //
-  //   if ( PROMPT_TYPE === 'PROMPT_CONFIRM' && e.which !== 13 ) {
-  //     this.refs.input.style.display = 'none';
-  //     inputValue = this.refs.hiddenInputField.value;
-  //     this.html.removeChild(inputField);
-  //     if ( typeof(callback) === 'function' ) {
-  //       if ( PROMPT_TYPE === 'PROMPT_CONFIRM' ) {
-  //         callback(inputValue.toUpperCase()[0] === 'Y' ? true : false);
-  //       } else {
-  //         callback(inputValue);
-  //       }
-  //     }
-  //   } else if ( e.which === 13 && this.state.shouldDisplayInputText ) {
-  //     this.refs.input.style.display = 'none';
-  //     inputValue = this.refrs.hiddenInputField.value;
-  //     this.print(inputValue);
-  //     callback(inputValue);
-  //   }
-  // }
-  //
-  //   if ( firstPrompt ) {
-  //     firstPrompt = false;
-  //     setTimeout( () => { inputField.focus();	}, 50)
-  //   } else {
-  //     inputField.focus();
-  //   }
-  // }
-
-/* HOW THE TERMINAL CLASS WORKS
- *  Contact Page instantiates the Terminal class
- *  Terminal constructor instantiates TerminalConstructor class
- *    instance has access to Terminal class methods fireCursorInterval() and promptInput()
- *    and variables firstPrompt and PROMPT_INPUT, PROMPT_PASSWORD, PROMPT_CONFIRM
- *
- *    html elements are created and attached to form the view of TerminalConstructor class instance
- *
- *    function in the React component calls the input() method on the terminal
- *    class which calls the promptInput() helper function which starts the chain
- *    of Promises and terminal prompts
- *
- *
- *
- *
- *
- *
-*/
+  /*--------------------------------------------------------------------------*/
+  /*--------------------------------RENDER METHOD-----------------------------*/
+  /*--------------------------------------------------------------------------*/
 
 
   render() {
@@ -420,8 +331,6 @@ gatherDataAndSend() {
       </div>
     )
   }
-
-
 }
 
 export default Terminal;
