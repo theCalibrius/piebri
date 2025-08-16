27d2bec4cb809e3a8bb33eca207c5c4c390ec30c
BP <at@at.at>
2025-08-16 15:43:24 -0700

    refactor: convert Home, Projects, Contact to arrow function componengs (no behavior change)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Terminal from './Terminal.jsx';
 import '../css/Contact.css';
 
-class Contact extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  componentDidMount() {}
-
-  render() {
-    return (
-      <div className="contactContent">
-        <div className="title">Contact</div>
-        <div className="contactForm">
-          <Terminal />
-        </div>
-      </div>
-    );
-  }
-}
+const Contact = () => (
+  <div className="contactContent">
+    <div className="title">Contact</div>
+    <div className="contactForm">
+      <Terminal />
+    </div>
+  </div>
+);
 
 export default Contact;
